4cdc0072c22cec2a3bf4c2ccbeaea598472c45e2
Thomas Tuegel <ttuegel@mailbox.org>
2017-11-08 14:25:50 -0600

    Parameterize over inputs

--- cabal.config.nix
+++ cabal.config.nix
@@ -3,13 +3,15 @@
 with nixpkgs;
 with pkgs;
 
+{ inputs ? [] }:
+
 stdenv.mkDerivation {
   name = "cabal.config";
   phases = "buildPhase";
-  buildInputs = [ zlib ];
+  buildInputs = inputs;
   preHook = ''
     declare -a cabalExtraLibDirs cabalExtraIncludeDirs
-    cabalProjectLocalEnvHook() {
+    cabalConfigLocalEnvHook() {
         if [ -d "$1/include" ]; then
             cabalExtraIncludeDirs+=("$1/include")
         fi
@@ -17,7 +19,7 @@ stdenv.mkDerivation {
             cabalExtraLibDirs+=("$1/lib")
         fi
     }
-    envHooks+=(cabalProjectLocalEnvHook)
+    envHooks+=(cabalConfigLocalEnvHook)
   '';
   buildPhase = ''
     eval $shellHook
@@ -31,9 +33,6 @@ stdenv.mkDerivation {
       ld-location: $(type -P ld)
       strip-location: $(type -P strip)
       tar-location: $(type -P tar)
-
-    program-options
-      gcc-options: $NIX_CFLAGS_COMPILE
     EOF
   '';
-}
+};
