1f207586b9430798bab40045dc595266442c6321
Thomas Tuegel <ttuegel@mailbox.org>
2017-11-27 16:26:33 -0600

    Allow custom ghc, alex, c2hs, happy, and hscolour

--- cabal.config.nix
+++ cabal.config.nix
@@ -3,12 +3,24 @@
 with nixpkgs;
 with pkgs;
 
-{ inputs ? (pkgs: []) }:
+{
+  inputs ? (pkgs: []),
+  alex ? (pkgs: pkgs.haskellPackages.alex),
+  c2hs ? (pkgs: pkgs.haskellPackages.c2hs),
+  ghc ? (pkgs: pkgs.haskell.compiler.ghc822),
+  happy ? (pkgs: pkgs.haskellPackages.happy),
+  hscolour ? (pkgs: pkgs.haskellPackages.hscolour),
+}:
 
 stdenv.mkDerivation {
   name = "cabal.config";
   phases = "buildPhase";
-  buildInputs = [ gcc zlib ] ++ (inputs pkgs);
+  buildInputs =
+    [
+      gcc pkgconfig zlib
+      (alex pkgs) (c2hs pkgs) (ghc pkgs) (happy pkgs) (hscolour pkgs)
+    ]
+    ++ (inputs pkgs);
   preHook = ''
     declare -a cabalExtraLibDirs cabalExtraIncludeDirs
     cabalConfigLocalEnvHook() {
@@ -25,9 +37,18 @@ stdenv.mkDerivation {
     eval $shellHook
     cat >"$out" <<EOF
     program-locations
+      alex-location: $(command -v alex)
       ar-location: $(command -v ar)
+      c2hs-location: $(command -v c2hs)
       gcc-location: $(command -v gcc)
+      ghc-location: $(command -v ghc)
+      ghc-pkg-location: $(command -v ghc-pkg)
+      haddock-location: $(command -v haddock)
+      happy-location: $(command -v happy)
+      hsc2hs-location: $(command -v hsc2hs)
+      hscolour-location: $(command -v hscolour)
       ld-location: $(command -v ld)
+      pkg-config-location: $(command -v pkg-config)
       strip-location: $(command -v strip)
       tar-location: $(command -v tar)
 
